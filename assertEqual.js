d21b3c51de64161d4c0f7758f729c4f7534655a3
Nosa Agho <nosaoasis@gmail.com>
2023-04-15 00:36:12 +0100

    Code Refactor
    Refactored the code to implement the algorithm using ternary operators.

--- assertEqual.js
+++ assertEqual.js
@@ -1,9 +1,10 @@
 const assertEqual = (actual, expected) => {
   console.assert(actual === expected);
-  if (actual === expected) {
-    return `✅✅✅ Assertion Passed: ${actual} === ${expected}`
-  }
-  return `🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`
+  // if (actual === expected) {
+  //   return `✅✅✅ Assertion Passed: ${actual} === ${expected}`
+  // }
+  // return `🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`
+  return actual === expected ? `✅✅✅ Assertion Passed: ${actual} === ${expected}` : `🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`
 }
 
 console.log(assertEqual("Lighthouse Labs", "Bootcamp"))
