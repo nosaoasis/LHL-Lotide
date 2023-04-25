604d0ea0ba6a68c9172cc3c29ddd425c58fcc870
Nosa Agho <nosaoasis@gmail.com>
2023-04-25 14:47:55 +0100

    Added Map Function
    Implemented the map function to that has the characters of a higher-order function.

--- map.js
+++ map.js
@@ -1,11 +1,6 @@
 const map = (arr, callback) => {
-  console.log('array: ', arr);
-  console.log('callback: ', callback);
   const results = []
   for (let item of arr) {
-    console.log('item BEFORE: ', item);
-    console.log('item AFTER: ', callback(item));
-    console.log('---');
     results.push(callback(item))
   }
   return results
