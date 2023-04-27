851f51298d8c0427eb42ecf7b5cc9bf3dd906bbc
Nosa Agho <nosaoasis@gmail.com>
2023-04-27 16:15:14 +0100

    Refactored eqArrays Function
    Modularized the eqArrays function to allow for other files to import it and use it.

--- eqArrays.js
+++ eqArrays.js
@@ -1,8 +1,3 @@
-const assertEqual = (actual, expected) => {
-  console.assert(actual === expected);
-  return actual === expected ? `✅✅✅ Assertion Passed: ${actual} === ${expected}` : `🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`
-}
-
 const eqArrays = (arr1, arr2) => {
   if (arr1.length !== arr2.length) return false 
   for (let i = 0; i < arr1.length; i++) {
@@ -15,14 +10,4 @@ const eqArrays = (arr1, arr2) => {
   return true
 }
 
-console.log(eqArrays([[2, 3], [4]], [[2, 3], [4]])) // => true
-
-console.log(eqArrays([[2, 3], [4]], [[2, 3], [4, 5]])) // => false
-console.log(eqArrays([[2, 3], [4]], [[2, 3], 4])) // => false
-
-console.log("111", eqArrays([1, 2, 3], [1, 2, 3]))
-console.log("222", eqArrays([1, 2, 3], [3, 2, 1]))
-console.log("333", eqArrays(["1", "2", "3"], ["1", "2", "3"]))
-console.log("444", eqArrays(["1", "2", "3"], ["1", "2", 3]))
-
-console.log(assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true))
+module.exports = { eqArrays };
