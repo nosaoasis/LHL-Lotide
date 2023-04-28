deacac3267b018feae5a424f5c928943fe4862fd
Nosa Agho <nosaoasis@gmail.com>
2023-04-28 12:11:57 +0100

    Mocha and Chai for Middle Function
    Refactored the code to make use of the mocha and chai library for test cases and also included some test cases as well.

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,5 +1,14 @@
-const {assertArraysEqual} = require("../assertArraysEqual")
-const {middle} = require("../middle")
+const assert = require("chai").assert;
+const { middle } = require("../middle");
 
-console.log(assertArraysEqual(middle([1, 2, 3, 4]), [2, 3]), true)
-console.log(assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [2, 3]), [4, 3])
+describe("#middle", () => {
+  it("returns [2] for [1, 2, 3]", () => {
+    assert.deepEqual(middle([1, 2, 3]), [2]);
+  });
+  it("returns [] for ['5']", () => {
+    assert.deepEqual(middle(["5"]), []);
+  });
+  it("returns [3, 4] for [1, 2, 3, 4, 5, 6]", () => {
+    assert.deepEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]);
+  });
+});
